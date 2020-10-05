c02c30ed74256a344d0f5f73ae146e16c5b62bb6
pfister <francois.pfister@gmail.com>
2020-10-05 12:24:59 +0200

    corrigé 1 séance 2

--- go.bat
+++ go.bat
@@ -1,1 +1,5 @@
 D:\apps\eclipse2020\eclipse\eclipse.exe -data . -vm D:\java\jdk-11.0.8\bin\javaw.exe
+rem echo %JAVA_HOME%
+rem PATH=%PATH%;%JAVA_HOME%\bin\
+rem adapter D:\apps\eclipse2020\eclipse\
+rem si JAVA_HOME existe enlever -vm D:\java\jdk-11.0.8\bin\javaw.exe
---
+++ test1/src/main/java/test1/OneshotServer.java
@@ -0,0 +1,31 @@
+package test1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class OneshotServer {
+
+	static int PORT = 25;
+	static final boolean LOG = true;
+
+	private static void clog(String message) {
+		if (LOG)
+			System.out.println(message);
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (LOG)
+			clog("je suis le serveur, j'écoute sur le port " + PORT);
+		ServerSocket socketEcoute = new ServerSocket(PORT);
+		Socket socketService = socketEcoute.accept();
+		if (socketService!=null)
+		  clog("un client s'est connecté");
+		
+
+		if (LOG)
+			clog("terminé");
+	}
+
+}
