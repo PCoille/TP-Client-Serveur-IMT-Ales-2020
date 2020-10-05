f4c96a44f39d861e304d1bb11892fc512caf3f35
pfister <francois.pfister@gmail.com>
2020-10-05 12:50:46 +0200

    attention à gitignore

--- go.bat
+++ go.bat
@@ -3,3 +3,4 @@ rem echo %JAVA_HOME%
 rem PATH=%PATH%;%JAVA_HOME%\bin\
 rem adapter D:\apps\eclipse2020\eclipse\
 rem si JAVA_HOME existe enlever -vm D:\java\jdk-11.0.8\bin\javaw.exe
+rem attention, dans .gitignore ne pas laisser *.xml car cela supprime pom.xml => plus de maven
