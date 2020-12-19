edf79b9321285b5fe3bb60a3e2f4b6ad8b98f83b
PCoille <44122462+PCoille@users.noreply.github.com>
2020-12-19 12:43:30 +0100

    Cleaned working dir from other irrelevent projects

--- test1/closeport.bat
+++
@@ -1,3 +0,0 @@
-rem syntax closeport [portnumber]  example closeport 23
-set "port=0.0.0.0:%1"
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr "%port%" ^| findstr "LISTENING"') do taskkill /f /pid %%a
